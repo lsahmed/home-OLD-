6b440e96ef4feb40d94326d9bf4ac7d7db5a8dd5
lsahmeed <ardyop7@gmail.com>
2024-11-05 15:56:25 +0000

    chacched

--- db_con.py
+++ db_con.py
@@ -1,9 +1,12 @@
 import pymongo
 from bson.objectid import ObjectId
-import base64
 import os
+from dotenv import load_dotenv
 
-url = "mongodb+srv://contrailpy:contrailpy@cluster0.xpysx.mongodb.net/"
+
+load_dotenv()
+DB_PASS = os.getenv('DB_PASS')
+url = f"mongodb+srv://contrailpy:{DB_PASS}@cluster0.xpysx.mongodb.net/"
 client = pymongo.MongoClient(url)
 db = client['portfolio']
 
@@ -14,19 +17,19 @@ skills = db['skills']
 projects = db['projects']
 project = list(projects.find())
 
-filter = {"_id":ObjectId('672790caff384ce856517e51')}
-update = {
-    "$set": {
-        "git": "https://github.com/lsahmed/contrail"
-    }
-}
-result = projects.update_one(filter ,update)
+# filter = {"_id":ObjectId('672790caff384ce856517e51')}
+# update = {
+#     "$set": {
+#         "git": "https://github.com/lsahmed/contrail"
+#     }
+# }
+# result = projects.update_one(filter ,update)
 
 
-if result.modified_count > 0:
-    print(f'Document with ID  updated successfully.')
-else:
-    print(f'No document found with ID  or no changes made.')
+# if result.modified_count > 0:
+#     print(f'Document with ID  updated successfully.')
+# else:
+#     print(f'No document found with ID  or no changes made.')
 
 
 
