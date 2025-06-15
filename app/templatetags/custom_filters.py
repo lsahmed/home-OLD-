d83c62421ffd86cbd4ef337da961b5756869db31
Ahmed <ardyop7@gmail.com>
2025-06-15 16:39:38 +0000

    Shifted old to here

--- app/templatetags/custom_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-def split(value, arg):
-    """ Split the string by the given delimiter."""
-    return value.split(arg)
-
-register.filter('split',split)
--- home/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
-
-def index(request):
-    return HttpResponse("Webpage is loading!")
